47a1d0bcf63e44d2baf05e40c8b6564b92ee12c7
Abdur Rahman Jalil <abdur@jalil.no>
2021-11-04 11:11:11 +0100

    fix comments

--- CheckPoints/Checkpoint7/Checkpoint_7.js
+++ CheckPoints/Checkpoint7/Checkpoint_7.js
@@ -1,12 +1,12 @@
 // 1-1
 
-/* function getSecondWord(stringOfWords) {
-    var words = stringOfWords.split(' ');
+function getSecondWord(stringOfWords) {
+  var words = stringOfWords.split(' ');
 
-    return words[1]!==undefined ? words[1] : "Not enough words"; // 2 is the third element, 0, 1, 2 ...
-  }
-  
-getSecondWord('hello my good friend'); */
+  return words[1] !== undefined ? words[1] : "Not enough words"; // 2 is the third element, 0, 1, 2 ...
+}
+
+getSecondWord('hello my good friend');
 
 
 // 1-2
@@ -15,77 +15,77 @@ getSecondWord('hello my good friend'); */
 a = 1, b = 3 --> 13
 changing to +prompt(); makes it save prompt as number
 a = 1, b = 3 --> 4 */
-/* function addNumbers() {
-    var a = +prompt('First number');
-    var b = +prompt('Second number');
-    var result = a + b;
-    console.log(result);
-  }
-  
-addNumbers(); */
-/* 
-// 1-3
-function wordRepeat(){
-    var word = prompt('What is your favourite word?')
-    var repeat = +prompt('How many times do you want me to repeat it?')
-    var output = "";
-    for (let i = 0; i<repeat; i++){
-        let joint = ", "
-        if (i===repeat-1){
-            joint = "!"
-        }
-        output = output + word + joint
-    }
-    console.log(output);
+function addNumbers() {
+  var a = +prompt('First number');
+  var b = +prompt('Second number');
+  var result = a + b;
+  console.log(result);
 }
 
-wordRepeat(); */
+addNumbers();
+
+
+// 1-3
+function wordRepeat() {
+  var word = prompt('What is your favourite word?')
+  var repeat = +prompt('How many times do you want me to repeat it?')
+  var output = "";
+  for (let i = 0; i < repeat; i++) {
+    let joint = ", "
+    if (i === repeat - 1) {
+      joint = "!"
+    }
+    output = output + word + joint
+  }
+  console.log(output);
+}
+
+wordRepeat();
 
 
 // 1-4
-/* function extendString(inputString){
-    var words = inputString.split(" ").length;
-    var addToOutput = "";
-    if(words>10){
-        addToOutput = "????"
-    }
-    else if(words>=5){
-        addToOutput = "!!!!"
-    }
-    console.log(inputString + addToOutput);
+function extendString(inputString) {
+  var words = inputString.split(" ").length;
+  var addToOutput = "";
+  if (words > 10) {
+    addToOutput = "????"
+  } else if (words >= 5) {
+    addToOutput = "!!!!"
+  }
+  console.log(inputString + addToOutput);
 }
 
-extendString('Hei abdur, konsentrer deg om checkpointen'); */
+extendString('Hei abdur, konsentrer deg om checkpointen');
 
 // 1-7
 
-class Counter{
-    constructor(a, b) {
-        if (a > b){
-            this.roofValue = +a;
-            this.floorValue = +b;
-        }
-        else{
-            this.floorValue = +a;
-            this.roofValue = +b;
-        }
-        this.currentValue = a>b ? +b : +a;
+class Counter {
+  constructor(a, b) {
+    if (a > b) {
+      this.roofValue = +a;
+      this.floorValue = +b;
+    } else {
+      this.floorValue = +a;
+      this.roofValue = +b;
     }
+    this.currentValue = a > b ? +b : +a;
+  }
 
-    getValue(){
-        return this.currentValue;
-    }
+  getValue() {
+    return this.currentValue;
+  }
 
-    increment(){
-        if (this.currentValue < this.roofValue){
-            this.currentValue++
-        }
+  increment() {
+    if (this.currentValue < this.roofValue) {
+      this.currentValue++
     }
-    decrement(){
-        if (this.currentValue > this.floorValue){
-            this.currentValue--
-        }
+  }
+
+  decrement() {
+    if (this.currentValue > this.floorValue) {
+      this.currentValue--
     }
+  }
 }
 
 
--- CheckPoints/Checkpoint7/checkpoint_7_N2.js
+++ CheckPoints/Checkpoint7/checkpoint_7_N2.js
@@ -1,6 +1,6 @@
 // 2-1
 
-/*function padString(str, pad, len) {
+function padString(str, pad, len) {
   var outputString = ""
 
   if (pad !== "" && len !== 0) {
@@ -10,69 +10,70 @@
   }
 
   return outputString + str;
-}*/
+}
 
 // 2-2
 
-/*const families = [
-    {
-        father: 'John',
-        mother: 'Mary',
-        children: ['Frank']
-    },
-    {
-        father: 'Mark',
-        mother: 'Linda',
-        children: ['Billy', 'Nina']
-    },
-    {
-        father: 'Chris',
-        mother: 'Lisa',
-        children: ['Dolly', 'Richard', 'Eva']
-    },
-    {
-        father: 'Herbert',
-        mother: 'Ursula',
-        children: ['Eleanor', 'Joshua', 'James']
-    }
+const families = [
+  {
+    father: 'John',
+    mother: 'Mary',
+    children: ['Frank']
+  },
+  {
+    father: 'Mark',
+    mother: 'Linda',
+    children: ['Billy', 'Nina']
+  },
+  {
+    father: 'Chris',
+    mother: 'Lisa',
+    children: ['Dolly', 'Richard', 'Eva']
+  },
+  {
+    father: 'Herbert',
+    mother: 'Ursula',
+    children: ['Eleanor', 'Joshua', 'James']
+  }
 ];
 
 
-var nameOfChildren = families.reduce(function(children, family) {
-    return children.concat(family.children);
-},[]);
+var nameOfChildren = families.reduce(function (children, family) {
+  return children.concat(family.children);
+}, []);
 
 var kidsNamesUppercaseIfManyKids = families
-  .filter(f => f.children.length>2)
-  .reduce(function(children, family) {
-      return children.concat(family.children);
-  },[])
-  .map(o => o.toUpperCase());*/
+  .filter(f => f.children.length > 2)
+  .reduce(function (children, family) {
+    return children.concat(family.children);
+  }, [])
+  .map(o => o.toUpperCase());
 
 
 // 2-3
-/*
 function createCounter(a, b) {
-    var floorValue = 0;
-    var roofValue = 0;
+  var floorValue = 0;
+  var roofValue = 0;
 
-    if (a > b){
-        roofValue = +a;
-        floorValue = +b;
-    }
-    else{
-        floorValue = +a;
-        roofValue = +b;
-    }
-    var currentValue = a>b ? +b : +a;
+  if (a > b) {
+    roofValue = +a;
+    floorValue = +b;
+  } else {
+    floorValue = +a;
+    roofValue = +b;
+  }
+  var currentValue = a > b ? +b : +a;
 
-    return {
-        getValue : function() { return currentValue; },
-        increment: function() { currentValue<roofValue ? currentValue++ : currentValue },
-        decrement : function() { currentValue>floorValue ? currentValue-- : currentValue},
-    }
+  return {
+    getValue: function () {
+      return currentValue;
+    },
+    increment: function () {
+      currentValue < roofValue ? currentValue++ : currentValue
+    },
+    decrement: function () {
+      currentValue > floorValue ? currentValue-- : currentValue
+    },
+  }
 }
-*/
-
-// 2-4
 
